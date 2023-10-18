f144de847490b5e0173860b466304b02d6523293
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-10-18 23:03:33 +0100

    LF

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,26 @@
-#include "sort.h"/*** bubble sort function * @array: array * @size_t: array size*/void bubble_sort(int *array, size_t size){	size_t i, n;	int tmp;	for (i = 0; i < size - 1; i++)	{		for (n = 0; n <= size - 2; n++)		{			if (array[i] > array[i + 1])			{				tmp = array[i];				array[i] = array [i+1];				array[i+1] = tmp;				print_array(array, size);			}		}	}}
+#include "sort.h"
+/**
+* bubble sort function
+ * @array: array
+ * @size_t: array size
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, n;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (n = 0; n <= size - 2; n++)
+		{
+			if (array[i] > array[i + 1])
+			{
+
+				tmp = array[i];
+				array[i] = array [i+1];
+				array[i+1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
