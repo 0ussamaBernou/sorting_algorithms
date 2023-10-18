5466c3f5fe2e1d68dcd6fb3304c3168969cf6ba0
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-10-18 21:48:42 +0100

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,13 +13,17 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, swaped;
 
+	/* Repeat n-1 times */
 	for (j = 0; j < size - 1; j++)
 	{
 		swaped = 0;
+		/* For i from 0 to n–2 */
 		for (i = 0; i <= size - 2; i++)
 		{
+			/* If numbers[i] and numbers[i+1] out of order */
 			if (array[i] > array[i + 1])
 			{
+				/* Swap them */
 				swaped = swap(&array[i], &array[i + 1]);
 				print_array(array, size);
 			}
