ea95c9d6aabe06aaa68379b0276c52d32516c5c3
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-10-21 23:42:04 +0100

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ * selection sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: The size of the array
+ **/
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+	/* Traverse through all array elements */
+	for (i = 0; i < size - 1; i++)
+	{
+		/* Assume the current index is the minimum */
+		min_idx = i;
+
+		/* Find the index of the minimum element in the remaining unsorted array */
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		/* Swap the found minimum element with the first element if it is not at the initial position */
+		if (min_idx != i)
+		{
+			/* Swap array[i] and array[min_idx] */
+			int temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+
+			/* Print the array after swapping */
+			print_array(array, size);
+		}
+	}
+}
