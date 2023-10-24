c1ed31f71eec840cae9241b92243039529dd07c3
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-10-24 19:54:42 +0100

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,56 +3,72 @@
 #include <stdio.h>
 
 /**
- * quick_sort - quick sort an array
- * @array: pointer to an array
- * @size: size of the array
+ * swap - Swaps two integer values.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
  *
+ * Return: 1 if swaped correctly
  */
-void quick_sort(int *array, size_t size)
+size_t swap(int *a, int *b)
 {
-	int i;
-	size_t j;
-	int pivot;
-	int const SIZE = size;
-	if (size <= 1)
-	{
-		return;
-	}
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+	return (1);
+}
 
-	pivot = array[size - 1];
-	i = -1;
+/**
+ * partition - Partitions the array and returns the pivot index.
+ * @array: Pointer to the array to be sorted.
+ * @lo: The low index of the subarray.
+ * @hi: The high index of the subarray.
+ * @size: size of the array
+ * Return: The pivot index.
+ */
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int pivot = array[hi];
+	int j;
+	int i = lo - 1;
 
-	for (j = 0; j < size - 1; j++)
+	for (j = lo; j < hi; j++)
 	{
 		if (array[j] <= pivot)
 		{
 			i++;
 			swap(&array[i], &array[j]);
-			print_array(array, SIZE);
 		}
 	}
-	i++;
-	swap(&array[i], &array[size - 1]);
-	print_array(array, SIZE);
 
-	quick_sort(array, (size_t)i);
-	quick_sort(&array[i + 1], (size_t)(size - i - 1));
+	swap(&array[i + 1], &array[hi]);
+	print_array(array, size);
+
+	return (i + 1);
 }
 
 /**
- * swap - swap two ints
- *
- * @a: first int
- * @b: second int
- *
- * Return: 1 indicating a swap
+ * quick_sort - Sorts an array using the Quick Sort algorithm.
+ * @array: Pointer to the array to be sorted.
+ * @size: The size of the array.
  */
-size_t swap(int *a, int *b)
+void quick_sort(int *array, size_t size)
 {
-	int tmp;
+	quick_sort_helper(array, 0, size - 1, size);
+}
 
-	tmp = *b;
-	*b = *a;
-	*a = tmp;
-	return 1;
+/**
+ * quick_sort_helper - Recursively sorts a subarray using Quick Sort.
+ * @array: Pointer to the array to be sorted.
+ * @lo: The low index of the subarray.
+ * @hi: The high index of the subarray.
+ * @size: size of the array
+ */
+void quick_sort_helper(int *array, int lo, int hi, size_t size)
+{
+	if (lo < hi)
+	{
+		int p = partition(array, lo, hi, size);
+		quick_sort_helper(array, lo, p - 1, size);
+		quick_sort_helper(array, p + 1, hi, size);
+	}
 }
--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
+
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
@@ -29,4 +31,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void quick_sort_helper(int *array, int lo, int hi, size_t size);
+
 #endif
