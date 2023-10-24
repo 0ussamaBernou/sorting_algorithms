0ac9a7834696ee0e53037378d6245047fcffc59f
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-10-24 20:05:15 +0100

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,30 +16,22 @@ void selection_sort(int *array, size_t size)
 		return;
 	}
 
-	/* Traverse through all array elements */
 	for (i = 0; i < size - 1; i++)
 	{
-		/* Assume the current index is the minimum */
 		min_idx = i;
 
-		/* Find the index of the minimum element in the remaining
-		 * unsorted array */
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
 
-		/* Swap the found minimum element with the first element */
-		/* if it is not at the initial position */
 		if (min_idx != i)
 		{
-			/* Swap array[i] and array[min_idx] */
 			temp = array[i];
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
 
-			/* Print the array after swapping */
 			print_array(array, size);
 		}
 	}
